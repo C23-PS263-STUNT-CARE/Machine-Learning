ffc74e0660100a1ffd84976340765ec2b86dcf43
afifdgr <afifdgr3@gmail.com>
2023-06-06 11:09:43 +0700

    # update

--- main.py
+++ main.py
@@ -1,52 +1,42 @@
 from flask import Flask, request, jsonify
-import pandas as pd
 import tensorflow as tf
+from preprocess import preprocess_data
 
 app = Flask(__name__)
 
-# Load the pre-trained model
-model = tf.keras.models.load_model('./model/model.h5')
+def predict_stunting(normalized_data):
+    # Load the TensorFlow model
+    model = "./model/model.h5"
+    model = tf.keras.models.load_model(model)
+    predictions = model.predict(normalized_data)
+    return predictions
 
-# Load the dataset
-dataset = pd.read_excel('./dataset/stunting_dataset.xlsx')
-
-# Mapping values for categorical columns
-sex_mapping = {'F': 0, 'M': 1}
-asi_mapping = {'No': 0, 'Yes': 1}
-
-# API endpoint for stunting detection
 @app.route('/predict', methods=['POST'])
-def predict_stunting():
-    # Get data from request
-    data = request.json
+def predict():
+    data = request.get_json()
+    sex = data['sex']
+    age = data['age']
+    birth_weight = data['birth_weight']
+    birth_length = data['birth_length']
+    body_weight = data['body_weight']
+    body_length = data['body_length']
+    asi_ekslusif = data['asi_ekslusif']
 
-    # Preprocess the input data
-    sex = sex_mapping[data['Sex']]
-    age = float(data['Age'])
-    birth_weight = float(data['Birth Weight'])
-    birth_length = float(data['Birth Length'])
-    body_weight = float(data['Body Weight'])
-    body_length = float(data['Body Length'])
-    asi_eksklusif = asi_mapping[data['ASI Eksklusif']]
+    normalized_data = preprocess_data(sex, age, birth_weight, birth_length, body_weight, body_length, asi_ekslusif)
+    predictions = predict_stunting(normalized_data)
+    rounded_prediction = round(predictions[0][0].item(), 1)
 
-    # Create a DataFrame for the input data
-    input_data = pd.DataFrame({
-        'Sex': [sex],
-        'Age': [age],
-        'Birth Weight': [birth_weight],
-        'Birth Length': [birth_length],
-        'Body Weight': [body_weight],
-        'Body Length': [body_length],
-        'ASI Eksklusif': [asi_eksklusif]
-    })
+    if rounded_prediction > 0.5:
+        stunting_status = "Stunting"
+    else:
+        stunting_status = "Not Stunting"
 
-    # Perform prediction
-    prediction = model.predict(input_data)
-    stunting = 'Yes' if prediction[0][0] > 0 else 'No'
+    result = {
+        'stunting_prediction': rounded_prediction,
+        'stunting_status': stunting_status
+    }
 
-    # Return the prediction as JSON response
-    response = {'Stunting': stunting}
-    return jsonify(response)
+    return jsonify(result)
 
-if (__name__ == "__main__"):
-     app.run(host="0.0.0.0", port = 5000, debug=False)
+if __name__ == '__main__':
+    app.run(debug=True)
